ba6feae3afc65e34efa87a772ad1c8344ac10edb
Hela Talbi <154261589+hela45@users.noreply.github.com>
2025-06-12 21:06:26 +0100

    Apply .gitignore to remove unwanted tracked files

--- SecureMark_HW/STM32CubeIDE/Debug/Application/SecureMark/monitor/th_api/th_libc.d
+++
@@ -1,4 +0,0 @@
-Application/SecureMark/monitor/th_api/th_libc.o: \
- C:/Users/helat/STM32CubeIDE/workspace_1.18.1/PFA_version2.0_securemark/securemark-v2-main/securemark-v2-main/monitor/th_api/th_libc.c \
- C:/Users/helat/STM32CubeIDE/workspace_1.18.1/PFA_version2.0_securemark/securemark-v2-main/securemark-v2-main/monitor/th_api/th_libc.h
-C:/Users/helat/STM32CubeIDE/workspace_1.18.1/PFA_version2.0_securemark/securemark-v2-main/securemark-v2-main/monitor/th_api/th_libc.h:
